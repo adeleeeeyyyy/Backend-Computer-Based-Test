2fd604a1f1d89d9042ae89c55b7ec50d1dad9521
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-04-04 21:05:41 +0700

    update controller soal

--- app/Http/Controllers/API/Guru/SoalController.php
+++ app/Http/Controllers/API/Guru/SoalController.php
@@ -2,10 +2,53 @@
 
 namespace App\Http\Controllers\API\Guru;
 
-use App\Http\Controllers\Controller;
+use App\Models\Soal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class SoalController extends Controller
 {
-    //
+    public function createSoal(Request $request, $tes_id) {
+        $request->validate([
+            'jenis_soal' => 'required|string',
+            'pertanyaan' => 'required|string',
+            'file_gambar'=> 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'poin' => 'required|integer',
+        ]);
+
+        $soal = DB::transaction(function () use ($tes_id, $request) {
+            $user = auth()->user()->guru_profile_id;
+            if ($user == null) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Anda belum login'
+                ], 400);
+            };
+
+            $soal_id = uniqid('soal_');
+
+            $file_gambar = $request->file('file_gambar')->store('gambar_soal', 'public');
+            return Soal::create([
+                'jenis_soal' => $request->jenis_soal,
+                'pertanyaan' => $request->pertanyaan,
+                'file_gambar' => $file_gambar,
+                'poin' => $request->poin,
+                'tes_id' => $tes_id,
+                'soal_id' => $soal_id
+            ]);
+        });
+        return response()->json([
+            'success'=> true,
+            'data' => $soal->only('pertanyaan')
+        ], 201);
+    }
+
+    public function showSoal() {
+        $soal = Soal::all();
+        return response()->json([
+            'success' => true,
+            'data' => $soal
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\API\Auth\AuthController;
-use App\Http\Controllers\API\Guru\TesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Guru\TesController;
+use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\Guru\SoalController;
 
 
 Route::get('/user', function (Request $request) {
@@ -21,12 +22,20 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
 
     Route::prefix('/guru')->group(function() {
-        Route::post('/createtes', [TesController::class,'createTes']);
-        Route::put('/updatetes/{tes_id}', [TesController::class,'updateTes']);
-        Route::get('/showtes', [TesController::class, 'showTes']);
-        Route::get('/showtes/{tes_id}', [TesController::class, 'showTesById']);
-        Route::put('/setstatus/{tes_id}', [TesController::class, 'setStatus']);
-        Route::delete('/deletetes/{tes_id}', [TesController::class, 'deleteTes']);
-        Route::delete('/deletealltes', [TesController::class, 'deleteAllTes']);
+        Route::prefix('/tes')->group(function() {
+            Route::post('/create', [TesController::class,'createTes']);
+            Route::put('/update/{tes_id}', [TesController::class,'updateTes']);
+            Route::get('/show', [TesController::class, 'showTes']);
+            Route::get('/show/{tes_id}', [TesController::class, 'showTesById']);
+            Route::put('/setstatus/{tes_id}', [TesController::class, 'setStatus']);
+            Route::delete('/delete/{tes_id}', [TesController::class, 'deleteTes']);
+            Route::delete('/deleteall', [TesController::class, 'deleteAllTes']);
+        });
+
+        Route::prefix('/soal')->group(function() {
+            Route::post('/create/{tes_id}', [SoalController::class,'createSoal']);
+            Route::get('/show', [SoalController::class, 'showSoal']);
+        });
+
     });
 });
