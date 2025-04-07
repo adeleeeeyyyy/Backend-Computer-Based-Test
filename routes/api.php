c44359a8a22a374047d89dab26aaa216ff5b5065
Hibakupoxzz <duospirits@gmail.com>
2025-04-07 22:27:59 +0700

    update dan hapus 1 soal

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,9 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::prefix('/soal')->group(function() {
             Route::post('/create/{tes_id}', [SoalController::class,'createSoal']);
             Route::get('/show/{tes_id}', [SoalController::class, 'showSoal']);
+            Route::put('/update/{soal_id}', [SoalController::class, 'updateSoal']);
             Route::delete('/deleteall/{tes_id}', [SoalController::class, 'deleteAllSoal']);
+            Route::delete('/delete/{soal_id}', [SoalController::class, 'deleteSoalindividu']);
         });
 
     });
