02357d188ef5d3d8e73e1b7499e11155d478d8cf
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-04-13 20:35:18 +0700

    dayum swoole

--- database/migrations/2025_04_12_123207_create_log_siswa_table.php
+++ database/migrations/2025_04_12_123207_create_log_siswa_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string("siswa_id");
             $table->timestamp(  "jam_mulai");
             $table->timestamp("jam_selesai");
-            
             $table->timestamps();
         });
     }
