6fcd83484b93086756eda0dc1cc80bf640fcd727
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-04-14 07:41:23 +0700

    refactor: Add try-catch blocks for error handling and transaction
    Implements try-catch blocks in GuruController, SoalController, and SiswaController to handle exceptions and improve error responses. Also, implements transaction in SiswaController and SoalController to ensure data consistency.
    Removes unused SoalController.

--- app/Http/Controllers/API/Guru/GuruController.php
+++ app/Http/Controllers/API/Guru/GuruController.php
@@ -9,28 +9,41 @@ use App\Http\Controllers\Controller;
 class GuruController extends Controller
 {
     public function seeSiswa() {
-        $siswa = User::select('id', 'username', 'email', 'role', 'status', 'siswa_profile_id')->where('role', '=', 'siswa')->get();
-        return response()->json([
-            'success' => true,
-            'message' => 'Data siswa berhasil diambil',
-            'data' => $siswa
-        ], 200);
+        try {
+            $siswa = User::select('id', 'username', 'email', 'role', 'status', 'siswa_profile_id')->where('role', '=', 'siswa')->get();
+            return response()->json([
+                'success' => true,
+                'message' => 'Data siswa berhasil diambil',
+                'data' => $siswa
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Gagal mengambil data siswa',
+                'data' => $e->getMessage()
+            ], 400);
+        }
     }
 
     public function seeSiswaByClass($class) {
-        $siswa = User::select('users.id', 'users.username', 'users.email', 'users.role', 'users.status', 'users.siswa_profile_id')
-            ->join('siswa_profiles', 'users.siswa_profile_id', '=', 'siswa_profiles.user_id')
-            ->where('users.role', '=', 'siswa')
-            ->where('siswa_profiles.kelas', '=', $class)
-            ->get();
+        try {
+            $siswa = User::select('users.id', 'users.username', 'users.email', 'users.role', 'users.status', 'users.siswa_profile_id')
+                ->join('siswa_profiles', 'users.siswa_profile_id', '=', 'siswa_profiles.user_id')
+                ->where('users.role', '=', 'siswa')
+                ->where('siswa_profiles.kelas', '=', $class)
+                ->get();
 
-        return response()->json([
-            'success' => true,
-            'message' => 'Data siswa berhasil diambil',
-            'data' => $siswa
-        ], 200);
+            return response()->json([
+                'success' => true,
+                'message' => 'Data siswa berhasil diambil',
+                'data' => $siswa
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Gagal mengambil data siswa',
+                'data' => $e->getMessage()
+            ], 400);
+        }
     }
-
-    
-    
 }
--- app/Http/Controllers/API/Guru/SoalController.php
+++ app/Http/Controllers/API/Guru/SoalController.php
@@ -18,7 +18,7 @@ class SoalController extends Controller
             'poin' => 'required|integer',
         ]);
 
-        $soal = DB::transaction(function () use ($tes_id, $request) {
+        try {
             $user = auth()->user()->guru_profile_id;
             if ($user == null) {
                 return response()->json([
@@ -30,7 +30,7 @@ class SoalController extends Controller
             $soal_id = uniqid('soal_');
 
             $file_gambar = $request->file('file_gambar')->store('gambar_soal', 'public');
-            return Soal::create([
+            $soal = Soal::create([
                 'jenis_soal' => $request->jenis_soal,
                 'pertanyaan' => $request->pertanyaan,
                 'file_gambar' => $file_gambar,
@@ -38,49 +38,76 @@ class SoalController extends Controller
                 'tes_id' => $tes_id,
                 'soal_id' => $soal_id
             ]);
-        });
-        return response()->json([
-            'success'=> true,
-            'data' => $soal->only('pertanyaan', 'soal_id', 'jenis_soal', 'file_gambar', 'poin')
-        ], 201);
+
+            return response()->json([
+                'success'=> true,
+                'data' => $soal->only('pertanyaan', 'soal_id', 'jenis_soal', 'file_gambar', 'poin')
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 
     public function showSoal($tes_id) {
-        $soal = Soal::where('tes_id', '=', $tes_id)->get();
-        return response()->json([
-            'success' => true,
-            'data' => $soal
-        ], 200);
+        try {
+            $soal = Soal::where('tes_id', '=', $tes_id)->get();
+            return response()->json([
+                'success' => true,
+                'data' => $soal
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 
     public function deleteAllSoal($tes_id) {
-        $soals = Soal::where('tes_id', '=', $tes_id)->get();
-        foreach ($soals as $soal) {
-            $soal->delete();
-        }
+        try {
+            $soals = Soal::where('tes_id', '=', $tes_id)->get();
+            foreach ($soals as $soal) {
+                $soal->delete();
+            }
 
-        return response()->json([
-            'success' => true,
-            'message' => 'Semua soal sudah dihapus'
-        ], 200);
+            return response()->json([
+                'success' => true,
+                'message' => 'Semua soal sudah dihapus'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 
     public function deleteSoalById(Request $request, $soal_id) {
-        $soal = Soal::where("soal_id", '=', $soal_id)->first();
+        try {
+            $soal = Soal::where("soal_id", '=', $soal_id)->first();
 
-        if (!$soal) {
+            if (!$soal) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Soal tidak ditemukan atau tidak termasuk dalam tes ini'
+                ], 404);
+            }
+
+            $soal->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Soal sukse dihapus'
+            ], 200);
+        } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
-                'message' => 'Soal tidak ditemukan atau tidak termasuk dalam tes ini'
-            ], 404);
+                'message' => $e->getMessage()
+            ], 400);
         }
-
-        $soal->delete();
-
-        return response()->json([
-            'success' => true,
-            'message' => 'Soal sukse dihapus'
-        ], 200);
     }
 
     public function updateSoal(Request $request, $soal_id) {
@@ -91,42 +118,34 @@ class SoalController extends Controller
             'poin' => 'nullable|integer',
         ]);
 
-        $soal = Soal::where('soal_id', '=', $soal_id)->first();
+        try {
+            DB::beginTransaction();
 
-        if (!$soal) {
+            $soal = Soal::where('soal_id', '=', $soal_id)->firstOrFail();
+
+            $soal->update($request->only('jenis_soal', 'pertanyaan', 'poin'));
+
+            // Menangani unggahan file jika gambar baru disediakan
+            if ($request->hasFile('file_gambar')) {
+                $file_gambar = $request->file('file_gambar')->store('gambar_soal', 'public');
+                $soal->update(['file_gambar' => $file_gambar]);
+            }
+
+            DB::commit();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Soal berhasil diperbarui',
+                'data' => $soal->only('pertanyaan', 'soal_id', 'jenis_soal', 'file_gambar', 'poin')
+            ], 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+
             return response()->json([
                 'success' => false,
-                'message' => 'Soal tidak ditemukan'
-            ], 404);
+                'message' => 'Gagal memperbarui soal',
+                'data' => $e->getMessage()
+            ], 400);
         }
-
-        // Update jika disediakan dalam permintaan
-        if ($request->has('jenis_soal')) {
-            $soal->jenis_soal = $request->jenis_soal;
-        }
-
-        if ($request->has('pertanyaan')) {
-            $soal->pertanyaan = $request->pertanyaan;
-        }
-
-        if ($request->has('poin')) {
-            $soal->poin = $request->poin;
-        }
-
-        // Menangani unggahan file jika gambar baru disediakan
-        if ($request->hasFile('file_gambar')) {
-            $file_gambar = $request->file('file_gambar')->store('gambar_soal', 'public');
-            $soal->file_gambar = $file_gambar;
-        }
-
-        $soal->save();
-
-        return response()->json([
-            'success' => true,
-            'message' => 'Soal berhasil diperbarui',
-            'data' => $soal->only('pertanyaan', 'soal_id', 'jenis_soal', 'file_gambar', 'poin')
-        ], 200);
     }
-
-    
 }
